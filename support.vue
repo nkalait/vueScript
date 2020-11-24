1e810378130af80805918c3b05c328511070ef9e
mnsekoli <mnsekoli@gmail.com>
2020-11-24 18:49:28 +0200

    Create support.vue
    initial commit

---
+++ support.vue
@@ -0,0 +1,62 @@
+<template>
+    <div class="w-100">
+       <h1>Support</h1> 
+   
+        <form >
+            <div class="form-row">
+                <div class="col-md-6 mb-3">
+                    <label for="item">Enter Item:</label>
+                    <input type="text" class="form-control form-control-lg" id="item" required>
+                    
+                </div>
+                <div class="col-md-6 mb-3">
+                    <label for="department">Select Department:</label>
+                    <select class="custom-select custom-select-lg" id="department">
+                        <option selected disabled value="">Choose...</option>
+                        <option
+                         v-for="dep in department" 
+                         v-bind:key="dep.id" 
+                        >
+                            {{dep.name}}
+                        </option>
+                    </select>
+                </div>
+            </div>
+            <button class="btn btn-primary" type="submit">Add Item</button>
+        </form>
+
+       
+    </div>  
+</template>
+
+
+
+<script>
+export default {
+    data: function(){
+        return{
+            department: []
+        }
+    },
+    
+    mounted(){
+        this.getDepartment();          
+    },
+
+    methods: {
+        getDepartment(){
+            axios.get('/api/department')
+            .then((res) => {this.department = res.data.data
+            })
+            .catch((error) =>{
+                console.log(error)
+            });
+        
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
